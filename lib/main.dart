e939c59acbe2c47c1613e56ea7241b11ef853014
ugur.narman <ugur.narman@hepsiburada.com>
2021-01-29 19:10:03 +0300

    new learning v3

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,43 @@ class MainPage extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Colors.blueGrey,
-          appBar: AppBar(
-            backgroundColor: Colors.blueGrey[900],
-            title: Text(
-              'Diamond',
-            ),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage(
+                    "https://ca.slack-edge.com/T029AES6D-U019V0VV8RY-36cc4c955827-512"),
+              ),
+              Text(
+                'Uğur Narman',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'pasificoRegular',
+                ),
+              ),
+              Container(
+                width: 250,
+                height: 3,
+                color: Colors.teal.shade100,
+              ),
+              Text(
+                'SOFTWARE DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'robotoMonoRegular',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              print('Selamin Aleyküm');
-            },
-            child: Icon(
-              Icons.add,
-            ),
-          ),
-          body: Center(
-            child: Image(
-              image: AssetImage('images/diamond.png'),
-            ),
-          )),
+        ),
+      ),
     );
   }
 }
